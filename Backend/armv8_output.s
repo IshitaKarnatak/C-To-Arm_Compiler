9e5d0d418e3d531248b02a3fc98a1982f35ac3c8
IshitaKarnatak <ishitakarnatak2404@gmail.com>
2025-06-14 12:30:13 +0530

    ARM Code Generation

---
+++ Backend/armv8_output.s
@@ -0,0 +1,9 @@
+Lexical Errors:
+There is no lexical errors.
+
+Syntax Errors:
+#7 : Syntax Error! Missing "NUM"
+
+Semantic Errors:
+#7 : Semantic Error! 'factorial' is not defined.
+#12 : Semantic Error! 'factorial' is not defined.
